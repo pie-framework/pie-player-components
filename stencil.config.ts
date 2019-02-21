5e76051da3d98704c0c243bdd0199143442285d5
evaneus <evan.eustace@gmail.com>
2019-02-21 09:56:40 -0500

    fix: add pie-loader

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,7 +2,7 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'pie-player-components',
-  bundles: [ { components: [ 'pie-author','pie-player' ] } ],
+  bundles: [ { components: [ 'pie-author','pie-player','pie-loader' ] } ],
   outputTargets:[
     { type: 'dist' },
     { type: 'docs' },
