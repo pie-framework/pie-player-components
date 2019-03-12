d1f7bcc3255e3a262149d7f3afd1b3a57dfb603c
evaneus <evan.eustace@gmail.com>
2019-03-12 17:16:50 -0400

    chore: make generic model

--- src/polyfill-elements.ts
+++ src/polyfill-elements.ts
@@ -6,39 +6,14 @@ createConfigFunctions['@pie-element/multiple-choice'] = model => {
   console.warn(`polyfilling createConfigFunction --- @pie-elements/mulitple-choice`);
   return {...{
     element: 'pie-multiple-choice',
-    prompt: 'Which of these northern European countries are EU members?',
+    prompt: 'Question Prompt',
     choiceMode: 'checkbox',
     keyMode: 'numbers',
     choices: [
       {
         correct: true,
-        value: 'sweden',
-        label: 'Sweden',
-        feedback: {
-          type: 'none',
-          value: ''
-        }
-      },
-      {
-        value: 'iceland',
-        label: 'Iceland',
-        feedback: {
-          type: 'none',
-          value: ''
-        }
-      },
-      {
-        value: 'norway',
-        label: 'Norway',
-        feedback: {
-          type: 'none',
-          value: ''
-        }
-      },
-      {
-        correct: true,
-        value: 'finland',
-        label: 'Finland',
+        value: 'choiceOne',
+        label: 'Choice One',
         feedback: {
           type: 'none',
           value: ''
