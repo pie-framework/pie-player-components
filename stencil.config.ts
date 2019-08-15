ca962205173709e258b53916f97982ef69fd7e84
evaneus <evan.eustace@gmail.com>
2019-08-15 11:10:50 -0400

    chore: include api docs in dist
    Also update copy  task for www (devt)

--- stencil.config.ts
+++ stencil.config.ts
@@ -2,12 +2,25 @@ import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 export const config: Config = {
   namespace: 'pie-player-components',
-  copy: [{ src: 'demo' }],
+  
   outputTargets: [
-    { type: 'dist' },
+    { 
+      type: 'dist' ,
+      copy: [
+        {
+          src: 'components/pie-author/readme.md',
+          dest: '../docs/pie-author.md'
+        },
+        {
+          src: 'components/pie-player/readme.md',
+          dest: '../docs/pie-player.md'
+        }
+      ]
+    },
     { type: 'docs-readme' },
     {
       type: 'www',
+      copy: [{ src: 'demo' }],
       serviceWorker: null // disable service workers
     }
   ],
