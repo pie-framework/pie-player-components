895101e1f099985a5e7d696758e2006f5b7a1ba2
evaneus <evan.eustace@gmail.com>
2019-08-19 11:06:11 -0400

    chore: format

--- src/components/pie-author/pie-author.tsx
+++ src/components/pie-author/pie-author.tsx
@@ -1,4 +1,4 @@
-import { Component, Element, Prop, State, Watch, Event, EventEmitter, Method, h } from '@stencil/core';
+import { Component, Element, Prop, State, Watch, Event, EventEmitter, Method, h, Listen } from '@stencil/core';
 import { PieContent, ItemConfig, PieElement, PieModel } from '../../interface';
 import { PieLoader } from '../../pie-loader';
 import { pieContentFromConfig } from '../../utils/utils';
@@ -17,18 +17,14 @@ import { addPackageToContent, addRubric } from '../../rubric-utils';
   shadow: false // shadow dom causes material ui problem
 })
 export class Author {
-
-
   _modelLoadedState: boolean = false;
 
   @Prop({ context: 'document' }) doc!: Document;
-
 
   /**
    * If set the player will add a rubric authoring interaction to the config
    */
   @Prop() addRubric: boolean;
-
 
   /**
    * Adds a preview view which will render the content in another tab as it may appear to a student or instructor.
@@ -44,30 +40,26 @@ export class Author {
    */
   @Prop() config: ItemConfig;
 
-
   /**
    * Emmitted when the model for the content has been updated within the ui due to user action.
    */
   @Event() modelUpdated: EventEmitter;
 
-
   /**
-   * Emmitted when the content models in the config have been set on the content 
+   * Emmitted when the content models in the config have been set on the content
    */
   @Event() modelLoaded: EventEmitter;
 
-
- 
   /**
-   * To customize the standard behaviour provided by interaction configuration views you can 
+   * To customize the standard behaviour provided by interaction configuration views you can
    * provide settings key-ed by the package name.  e.g.
-   * 
+   *
    * `{ '@pie-element/inline-choice': { promptLabel: 'Item Stem' } }`
-   * 
-   * The settings that are configurable for each authoring view are documented in 
+   *
+   * The settings that are configurable for each authoring view are documented in
    * the `@package-name/docs` folder for each package.
    */
-  @Prop() configSettings?: {[packageName:string]:Object}
+  @Prop() configSettings?: { [packageName: string]: Object };
 
   pieContentModel: PieContent;
 
@@ -76,10 +68,10 @@ export class Author {
   renderMarkup: String;
 
   getRenderMarkup(): string {
-    let markup = this.pieContentModel ? this.pieContentModel.markup : "";
+    let markup = this.pieContentModel ? this.pieContentModel.markup : '';
     if (markup) {
       Object.keys(this.pieContentModel.elements).forEach(key => {
-        markup = markup.split(key).join( key+'-config');
+        markup = markup.split(key).join(key + '-config');
       });
       return markup;
     }
@@ -87,7 +79,7 @@ export class Author {
 
   @Watch('config')
   async watchConfig(newValue, oldValue) {
-    if (newValue && !_isEqual(newValue,oldValue)) { 
+    if (newValue && !_isEqual(newValue, oldValue)) {
       try {
         this.elementsLoaded = false;
         this._modelLoadedState = false;
@@ -97,16 +89,13 @@ export class Author {
       } catch (error) {
         console.log(`ERROR ${error}`);
       }
-      
     }
   }
 
   addConfigTags(c: PieContent) {
     if (!c.markup && c.models) {
       const tags = c.models.map(model => {
-        return `<${model.element} id="${model.id}"></${
-          model.element
-        }-config>`;
+        return `<${model.element} id="${model.id}"></${model.element}-config>`;
       });
       c.markup = tags.join('');
     }
@@ -138,9 +127,9 @@ export class Author {
         if (this.pieContentModel.elements[pieElName]) {
           const elementId = el.getAttribute('id');
           if (!this.pieContentModel.models.find(m => m.id === elementId)) {
-            const model = {id: elementId, element: pieElName};
+            const model = { id: elementId, element: pieElName };
             this.pieContentModel.models.push(model);
-          }  
+          }
         }
       });
       tempDiv.remove();
@@ -149,14 +138,14 @@ export class Author {
       this.pieContentModel.models.map(model => {
         let pieEl: PieElement = this.el.querySelector(`[id='${model.id}']`);
         !pieEl && (pieEl = this.el.querySelector(`[pie-id='${model.id}']`));
-        
+
         if (pieEl) {
           const pieElName = pieEl.tagName.toLowerCase().split('-config')[0];
           const packageName = parseNpm(this.pieContentModel.elements[pieElName])
-              .name;
+            .name;
           pieEl.model = model;
           if (this.configSettings && this.configSettings[packageName]) {
-            pieEl.configuration = this.configSettings[packageName]
+            pieEl.configuration = this.configSettings[packageName];
           }
         }
       });
@@ -164,7 +153,6 @@ export class Author {
         this._modelLoadedState = true;
         this.modelLoaded.emit(this.pieContentModel);
       }
-      
     }
   }
 
@@ -176,19 +164,19 @@ export class Author {
 
   @Listen('model.updated')
   modelUpdatedHandler(e: ModelUpdatedEvent) {
-      // set the internal model
-      // emit a content-item level event with the model
-      if (this.pieContentModel && e.update) {
-        this.pieContentModel.models.forEach(m => {
-          if (m.id === e.update.id) {
-            Object.assign(m, e.update);
-          }
-        });
-    }
-        if (this._modelLoadedState) {
-      this.modelUpdated.emit(this.pieContentModel);
+    // set the internal model
+    // emit a content-item level event with the model
+    if (this.pieContentModel && e.update) {
+      this.pieContentModel.models.forEach(m => {
+        if (m.id === e.update.id) {
+          Object.assign(m, e.update);
         }
-      }  
+      });
+    }
+    if (this._modelLoadedState) {
+      this.modelUpdated.emit(this.pieContentModel);
+    }
+  }
 
   async componentDidLoad() {
     await this.afterRender();
@@ -208,37 +196,40 @@ export class Author {
   }
 
   async afterRender() {
-    if (this.pieContentModel && this.pieContentModel.markup && !this.elementsLoaded) {
-        this.elementsLoaded = await this.pieLoader.elementsHaveLoaded(this.el);
+    if (
+      this.pieContentModel &&
+      this.pieContentModel.markup &&
+      !this.elementsLoaded
+    ) {
+      this.elementsLoaded = await this.pieLoader.elementsHaveLoaded(this.el);
     }
   }
 
   /**
    * Utility method to add a `@pie-element/rubric` section to an item config when creating an item should be used before setting the config.
-   * 
+   *
    * @deprecated this method is for temporary use, will be removed at next major release
-   * 
+   *
    * @param config the item config to mutate
-   * @param rubricModel 
+   * @param rubricModel
    */
   @Method()
   async addRubricToConfig(config: ItemConfig, rubricModel?) {
     if (!rubricModel) {
       rubricModel = {
-        "id": "rubric",
-        "element": "pie-rubric",
-        "points": [
-          "",
-          "",        
-          "",        
-          ""        
-        ],
-        "maxPoints": 4,
-        "excludeZero": false
+        id: 'rubric',
+        element: 'pie-rubric',
+        points: ['', '', '', ''],
+        maxPoints: 4,
+        excludeZero: false
       };
     }
     const configPieContent = pieContentFromConfig(config);
-    addPackageToContent(configPieContent, '@pie-element/rubric', rubricModel as PieModel);
+    addPackageToContent(
+      configPieContent,
+      '@pie-element/rubric',
+      rubricModel as PieModel
+    );
     addRubric(configPieContent);
     return config;
   }
@@ -246,17 +237,24 @@ export class Author {
   render() {
     if (this.pieContentModel && this.pieContentModel.markup) {
       if (this.addPreview) {
-        return <pie-preview-layout config={this.config} >
-          <div slot="configure">
-          <pie-spinner active={!this.elementsLoaded}><div innerHTML={this.getRenderMarkup()}></div>
-          </pie-spinner>
-          </div>
-        </pie-preview-layout>
+        return (
+          <pie-preview-layout config={this.config}>
+            <div slot="configure">
+              <pie-spinner active={!this.elementsLoaded}>
+                <div innerHTML={this.getRenderMarkup()} />
+              </pie-spinner>
+            </div>
+          </pie-preview-layout>
+        );
       } else {
-        return <pie-spinner active={!this.elementsLoaded}><div innerHTML={this.getRenderMarkup()} /></pie-spinner>;
+        return (
+          <pie-spinner active={!this.elementsLoaded}>
+            <div innerHTML={this.getRenderMarkup()} />
+          </pie-spinner>
+        );
       }
     } else {
-      return <pie-spinner></pie-spinner>;
+      return <pie-spinner />;
     }
   }
 }
