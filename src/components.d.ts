058e7e4183bfb255731303002cdc430a24ebdab4
ed eustace <ed.eustace@gmail.com>
2019-09-18 12:26:51 +0100

    feat: add image support (#13) [ch1750]
    By default image support will use inline images as dataURLs. But the
    outer context can set `imageSupport: ExternalImageSupport` to handle it.
    
    Note: @pie-lib/editable-html needs an upgrade cos it's delete button
    isn't working properly.

--- src/components.d.ts
+++ src/components.d.ts
@@ -12,6 +12,9 @@ import {
   PieItemElement,
   PieModel,
 } from './interface';
+import {
+  ExternalImageSupport,
+} from './components/pie-author/dataurl-image-support';
 
 export namespace Components {
   interface PieAuthor {
@@ -38,6 +41,10 @@ export namespace Components {
     * To customize the standard behaviour provided by interaction configuration views you can provide settings key-ed by the package name.  e.g.  `{ '@pie-element/inline-choice': { promptLabel: 'Item Stem' } }`  The settings that are configurable for each authoring view are documented in the `@package-name/docs` folder for each package.
     */
     'configSettings'?: { [packageName: string]: Object };
+    /**
+    * external providers can set this if they need to upload the assets to the cloud etc. by default we use data urls
+    */
+    'imageSupport': ExternalImageSupport;
   }
   interface PieLoader {
     /**
@@ -60,7 +67,7 @@ export namespace Components {
   }
   interface PiePlayer {
     /**
-    * Simulates a correct response for the item. This property will only have this effect if the `hosted` property is  false and player is running client-side-only.
+    * Simulates a correct response for the item. This property will only have this effect if the `hosted` property is false and player is running client-side-only.
     */
     'addCorrectResponse': boolean;
     /**
@@ -180,6 +187,10 @@ declare namespace LocalJSX {
     */
     'configSettings'?: { [packageName: string]: Object };
     /**
+    * external providers can set this if they need to upload the assets to the cloud etc. by default we use data urls
+    */
+    'imageSupport'?: ExternalImageSupport;
+    /**
     * Emmitted when the content models in the config have been set on the content
     */
     'onModelLoaded'?: (event: CustomEvent<any>) => void;
@@ -204,7 +215,7 @@ declare namespace LocalJSX {
   }
   interface PiePlayer extends JSXBase.HTMLAttributes<HTMLPiePlayerElement> {
     /**
-    * Simulates a correct response for the item. This property will only have this effect if the `hosted` property is  false and player is running client-side-only.
+    * Simulates a correct response for the item. This property will only have this effect if the `hosted` property is false and player is running client-side-only.
     */
     'addCorrectResponse'?: boolean;
     /**
@@ -236,7 +247,7 @@ declare namespace LocalJSX {
     */
     'onResponseCompleted'?: (event: CustomEvent<any>) => void;
     /**
-    * Emmitted when any interaction in the set of interactions being rendered has been mutated by user action.  The `Event.detail` property contains a `complete` property. If true, this indicates that enough data has been gathered by the interaciton to constitute a response. For example, in a plot line questsion where a user had to plot three points to plot the line,  the `complete` propery would be false if 1 or 2 points had been added, but true if all three had.
+    * Emmitted when any interaction in the set of interactions being rendered has been mutated by user action.  The `Event.detail` property contains a `complete` property. If true, this indicates that enough data has been gathered by the interaciton to constitute a response. For example, in a plot line questsion where a user had to plot three points to plot the line, the `complete` propery would be false if 1 or 2 points had been added, but true if all three had.
     */
     'onSession-changed'?: (event: CustomEvent<any>) => void;
     /**
