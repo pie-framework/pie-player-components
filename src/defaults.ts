8b4fb6d095a579ae991e41915655e3ef407f487b
Elod-Arpad Szopos <elodszopos@hotmail.com>
2019-11-05 19:19:03 +0200

    feat: call imageSupport.delete function

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,1 +1,2 @@
-export const BUILD_SERVICE_BASE = "https://pits-dot-pie-dev-221718.appspot.com/bundles/";
+export const BUILD_SERVICE_BASE =
+  "https://pits-dot-pie-dev-221718.appspot.com/bundles/";
