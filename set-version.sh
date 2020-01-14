99cee1770214ca74b6b7c83e77259a47ff412f5d
ed eustace <ed.eustace@gmail.com>
2020-01-14 16:25:59 +0000

    fix: postinstall when not root repo

--- set-version.sh
+++ set-version.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-echo "setting version.ts to $1"
-cat > ./src/version.ts <<- EOF
+
+
+if [ -d ./src ]; then
+  echo "setting version.ts to $1"
+  cat > ./src/version.ts <<- EOF
 // auto generated on release
 export const VERSION = "$1";
 EOF
+else
+  echo "pie-player-components non-root-install do nothing"
+fi
