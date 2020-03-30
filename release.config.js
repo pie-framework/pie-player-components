79d65007fe475a6365466aca10330584ba40a339
evaneus <evan.eustace@gmail.com>
2020-03-30 14:14:46 -0400

    feat: fix release config PD-74

--- release.config.js
+++ release.config.js
@@ -20,12 +20,12 @@ module.exports = {
     ],
     "@semantic-release/github",
     ["@pie-api/semantic-release-jira-releases", {
-      "projectId": "13788",
-      "released": "true",
-      "releaseDate": "true",
-      "releaseNameTemplate": "pie player v${version}",
-      "jiraHost": "illuminate.atlassian.net",
-      "ticketPrefixes": ["PD"]
+      projectId: 13788,
+      released: true,
+      releaseDate: true,
+      releaseNameTemplate: "pie player v${version}",
+      jiraHost: "illuminate.atlassian.net",
+      ticketPrefixes: ["PD"]
     }]
   
   ]
