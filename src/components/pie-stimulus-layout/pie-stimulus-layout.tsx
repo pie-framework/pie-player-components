f567ccd45063c79a18d32677f5b02f76be1e697c
lakatosandrei <lakatosandrei@gmail.com>
2020-04-02 19:30:05 +0300

    fix(player): separate scrolling containers in stimulus layout [ch5777] (#45) IBX-3339

--- src/components/pie-stimulus-layout/pie-stimulus-layout.tsx
+++ src/components/pie-stimulus-layout/pie-stimulus-layout.tsx
@@ -2,6 +2,28 @@ import { Component, h } from '@stencil/core';
 
 @Component({ tag: 'pie-stimulus-layout' , styleUrl: 'pie-stimulus-layout.css'})
 export class PieStimulusLayout {
+  static handleElements() {
+    const el = document.querySelector('#pie-stimulus-container') as HTMLElement;
+
+    if (el) {
+      const boundRect = el.getBoundingClientRect();
+
+      el.style.height = `calc(100vh - ${boundRect.top}px`;
+    }
+  }
+
+  componentDidRender() {
+    PieStimulusLayout.handleElements();
+  }
+
+  componentDidLoad() {
+    PieStimulusLayout.handleElements();
+  }
+
+  componentDidUpdate() {
+    PieStimulusLayout.handleElements();
+  }
+
   render() {
     return (
       <div id="pie-stimulus-container">
