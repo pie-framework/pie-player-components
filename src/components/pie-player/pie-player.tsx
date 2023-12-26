93f8d5fefc88339d37d3a99a861732c81b004b2c
andreeapescar <andreeaflaviapescar@gmail.com>
2023-12-26 14:33:06 +0200

    feat: use pie-lib/pie-toolbox instead of pie-lib/math-rendering

--- src/components/pie-player/pie-player.tsx
+++ src/components/pie-player/pie-player.tsx
@@ -1,5 +1,6 @@
 import {SessionChangedEvent} from "@pie-framework/pie-player-events";
-import * as mr from "@pie-lib/math-rendering";
+import { _dll_pie_lib__pie_toolbox_math_rendering as mr } from "@pie-lib/pie-toolbox-module/module";
+
 import {
   Component,
   Element,
