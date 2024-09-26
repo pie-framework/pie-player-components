92f395faf165c4133cbdd77233d538e387e8d609
andreeapescar <andreeaflaviapescar@gmail.com>
2024-09-26 17:12:59 +0300

    fix: add back math-rendering-accessible

--- src/components/pie-author/pie-author.tsx
+++ src/components/pie-author/pie-author.tsx
@@ -8,8 +8,7 @@ import {
 } from "@pie-framework/pie-configure-events";
 
 import {
-  _dll_pie_lib__pie_toolbox_math_rendering,
-  // _dll_pie_lib__pie_toolbox_math_rendering_accessible
+  _dll_pie_lib__pie_toolbox_math_rendering_accessible
 } from "@pie-lib/pie-toolbox-math-rendering-module/module";
 import {Component, Element, Event, EventEmitter, h, Method, Prop, State, Watch} from "@stencil/core";
 import cloneDeep from "lodash/cloneDeep";
@@ -740,10 +739,7 @@ export class Author {
   }
 
   private renderMath() {
-    setTimeout(() => {
-      _dll_pie_lib__pie_toolbox_math_rendering.renderMath(this.el);
-      // _dll_pie_lib__pie_toolbox_math_rendering_accessible.renderMath(this.el);
-    }, 50);
+    _dll_pie_lib__pie_toolbox_math_rendering_accessible.renderMath(this.el);
   }
 
   async afterRender() {
